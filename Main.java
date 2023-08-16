8cc60cbb1723941e2d3e466cb100245221362d5d
roboy05 <royroboy@gmail.com>
2023-08-16 11:25:59 +0530

    see changes

--- Main.java
+++ Main.java
@@ -3,12 +3,12 @@ public class Main {
 
   public static void main(String[] args) {
 
-    int num = 31;   
+    int num = 3211;   
     boolean flag = false;
     for (int i = 2; i <= num / 2; ++i) {
       // condition for nonprime number
       if (num % i == 0) {
-        flag = true;
+        flag = true; 
         break;
       }
     }
